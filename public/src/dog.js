9fbe850feec32102659b3817c3d8cabb138bbdf3
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-10-02 17:03:52 -0400

    practice with classes

---
+++ public/src/dog.js
@@ -0,0 +1,21 @@
+class Dog {
+  static allDogs = []
+
+  constructor(name){
+    this.name = name;
+    Dog.allDogs.push(this)
+    this.appendDog()
+  }
+
+  //add new dogs to the dom
+  appendDog(){
+    let div = document.getElementById("dog-div")
+    let ele = document.createElement("h2")
+    ele.innerText = this.name;
+    div.appendChild(ele)
+  }
+
+  bark(){
+    console.log(`${this.name} says Woof!`)
+  }
+}
